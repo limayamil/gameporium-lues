d701ebbfa8873cff19c3ebc41e6a961676ed39aa
Yamil Lues <yamillues@gmail.com>
2022-09-13 13:31:39 -0300

    Fix: imágenes rotas

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,17 +5,17 @@ export const ProductList = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et placeat porro sit reprehenderit accusantium itaque, quod ad, eum non corrupti odit reiciendis? Tenetur, beatae inventore animi minima iusto unde aliquid!",
     image:
-      "https://mundofix.com/199912-home_default/placa-de-video-colorful-rtx3070-ti-igame-ultra-w-oc-8gb-gddr6.jpg",
+      "https://mundofixar.vtexassets.com/arquivos/ids/264581-800-auto?v=637983317000730000&width=800&height=auto&aspect=true",
     price: 241669,
     stock: 10,
   },
   {
     id: 2,
-    name: "Placa de Video Colorful RTX3080",
+    name: "Placa de Video Colorful RTX3070 Ti",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et placeat porro sit reprehenderit accusantium itaque, quod ad, eum non corrupti odit reiciendis? Tenetur, beatae inventore animi minima iusto unde aliquid!",
     image:
-      "https://mundofix.com/344185-home_default/placa-de-video-colorful-rtx3080-igame-ultra-oc-lhr-10gb-gddr6x.jpg",
+      "https://mundofixar.vtexassets.com/arquivos/ids/266620-800-auto?v=637983507773370000&width=800&height=auto&aspect=true",
     price: 203479,
     stock: 30,
   },
@@ -25,7 +25,7 @@ export const ProductList = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et placeat porro sit reprehenderit accusantium itaque, quod ad, eum non corrupti odit reiciendis? Tenetur, beatae inventore animi minima iusto unde aliquid!",
     image:
-      "https://mundofix.com/494440-home_default/placa-de-video-colorful-rtx3060-ti-nb-duo-v2-lhr-8gb-gddr6.jpg",
+      "https://mundofixar.vtexassets.com/arquivos/ids/266185-800-auto?v=637983464536100000&width=800&height=auto&aspect=true",
     price: 154059,
     stock: 40,
   },
